4f71c50be4c028c26a1025a6972dda5acf174525
Theshoke <kesinovic.josip@gmail.com>
2019-12-14 08:22:30 +0100

    Deleted all

--- Home/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView #built-in TemplateView kako bi prikazali template
-
-# Create your views here.
-class HomePageView(TemplateView):
-    template_name = 'home.html'
